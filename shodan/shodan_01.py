e1a248853abcd78379378b43c08eeb0e7c0010c2
felipeadtr <felipe.adtr@gmail.com>
2021-09-15 17:16:13 -0400

    AULA SHODAN

---
+++ shodan/shodan_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import shodan
+shodan_mykey='nkGd8uVE4oryfUVvioprswdKGmA5InzZ'
+shodan_api = shodan.Shodan(shodan_mykey)
+shodan_target='179.191.78.194'
+shodan_host = shodan_api.host(shodan_target)
+
+print('IP alvo:',shodan_host['ip_str'])
+print('Organizacao:', shodan_host.get('org','n/a'))
+print('Sistema Operacional:', shodan_host.get('os','n/a'))
